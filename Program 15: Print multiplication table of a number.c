2c7e46c23ec24a2faa565d03a73fa14c59180a71
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-03 00:42:16 +0530

    Create Program 15: Print multiplication table of a number.c

---
+++ Program 15: Print multiplication table of a number.c
@@ -0,0 +1,11 @@
+        # include <stdio.h>
+        void main(){
+        int a,n;
+        printf("enter your number");
+        scanf("%d",&n);
+       for(a=1; a<=10; a++){
+     
+       printf("%d * %d =%d\n",n ,a ,n*a);
+        
+    }
+        }
