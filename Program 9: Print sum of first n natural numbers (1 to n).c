37d7ffc96218609134362966240d84c5109d26f0
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-03 00:22:40 +0530

    Create Program 9: Print sum of first n natural numbers (1 to n).c

---
+++ Program 9: Print sum of first n natural numbers (1 to n).c
@@ -0,0 +1,11 @@
+        # include <stdio.h>
+        void main(){
+        int a,n,sum=0;
+        printf("enter your number");
+        scanf("%d",&n);
+       for(a=1; a<=n; a++){
+    
+    sum+= a;
+       }
+       printf("Sum of first %d natural numbers is %d\n", n, sum);
+        }
