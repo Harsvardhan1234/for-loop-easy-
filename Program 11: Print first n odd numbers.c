072c3dcb4b1cc44c0ee7e4686b83d3bc99864bc4
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-03 00:28:45 +0530

    Create Program 11: Print first n odd numbers.c

---
+++ Program 11: Print first n odd numbers.c
@@ -0,0 +1,12 @@
+        # include <stdio.h>
+        void main(){
+        int a,n;
+        printf("enter your number");
+        scanf("%d",&n);
+       for(a=1; a<=n; a++){
+    
+ 
+       
+       printf("%d\n",(2*a)+1);
+        }
+    }
