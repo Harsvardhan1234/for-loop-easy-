4c1cdd7805260bc0c8dc136ea4d74c2caa9bd0b7
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-03 01:06:31 +0530

    Create Program 24: Print sum of first n odd numbers.c

---
+++ Program 24: Print sum of first n odd numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    int i, n ,sum = 0;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        sum += 2 * (i+1) ; }
+        printf("Sum of first %d even numbers is %d\n", n, sum);
+    
+}
+
