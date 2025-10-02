1bcff1887dfaa5d82a5506d3fd47ee0a1fe75819
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-03 00:34:41 +0530

    Create Program 13: Print numbers divisible by 5 from 1 to n.c

---
+++ Program 13: Print numbers divisible by 5 from 1 to n.c
@@ -0,0 +1,11 @@
+        # include <stdio.h>
+        void main(){
+        int a,n;
+        printf("enter your number");
+        scanf("%d",&n);
+       for(a=1; a<=n; a++){
+        if( a % 5 == 0){
+       printf("%d\n",a);
+        }
+    }
+        }
