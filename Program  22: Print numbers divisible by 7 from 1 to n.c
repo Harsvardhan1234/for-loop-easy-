541edfa9685bc8a043acda12dd13ae4fddde2585
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-03 00:57:46 +0530

    Create Program  22: Print numbers divisible by 7 from 1 to n.c

---
+++ Program  22: Print numbers divisible by 7 from 1 to n.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    int i, n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        if(i % 7==0){
+        printf("%d\n", i);
+    }
+}
+
+}
